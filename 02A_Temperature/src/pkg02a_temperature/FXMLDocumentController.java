34c59bfabbf0197fb8cf36fb3a6e653925cf7c21
kiss j gabi <b6@b6.hu>
2019-10-05 19:14:21 +0200

    add 02A - Homerseklet java-fx project

---
+++ 02A_Temperature/src/pkg02a_temperature/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg02a_temperature;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+/**
+ *
+ * @author t1
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label lblFok;
+
+    @FXML
+    private Slider sldCsuszka;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        sldCsuszka.valueProperty().addListener((o, regi, uj) -> {
+            int c = uj.intValue();
+            int f = (int) Math.round(9.0 / 5.0 * c + 32);
+            lblFok.setText(c + " °C = " + f + " °F");
+        });
+    }
+
+}
---
+++ 02A_Temperature/src/pkg02a_temperature/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg02a_temperature;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author t1
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Hőmérséklet kalkulátor");
+        stage.getIcons().add(new Image("homer.png"));
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
